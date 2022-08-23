5c401b11a718a669f4829822cf0f365e4a0435c1
DevRefaul <devrefaul@gmail.com>
2022-08-23 18:35:40 +0600

    fitness concern website created with some responsiveness

---
+++ app.js
@@ -0,0 +1,8 @@
+
+document.getElementById('move-top').addEventListener('click', function () {
+    
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+
+
+})
